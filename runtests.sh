15d6c80db4c576d7f375fafe12f4611c246514de
Vojtech Bocek <vojtech.bocek@avast.com>
2019-05-16 11:05:48 +0200

    Build on 32bit and on Windows in travis
    Updates #2

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eux
 
+if [ -n "${GIMME_OS:-}" ] && [ "$GIMME_OS" != "linux" ]; then
+    echo "Non-linux travis platform, skipping tests..."
+    exit 0
+fi
+
 if ! [ -d "apksig_for_tests" ]; then
     git clone --depth=1 -b oreo-mr1-iot-release https://android.googlesource.com/platform/tools/apksig apksig_for_tests
 else
