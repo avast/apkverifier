290c8f7fccf7a4a672bef41ccd5d69c4554a1d4d
Vojtěch Boček <vojtech.bocek@avast.com>
2021-03-01 11:17:18 +0100

    Fix asn1 tests

--- internal/asn1andr/asn1_test.go
+++ internal/asn1andr/asn1_test.go
@@ -379,20 +379,20 @@ var tagAndLengthData = []tagAndLengthTest{
 	{[]byte{0x00, 0x83, 0x01, 0x00}, false, tagAndLength{}},
 	{[]byte{0x1f, 0x85}, false, tagAndLength{}},
 	{[]byte{0x30, 0x80}, false, tagAndLength{}},
-	// Superfluous zeros in the length should be an error.
-	{[]byte{0xa0, 0x82, 0x00, 0xff}, false, tagAndLength{}},
+	// Superfluous zeros in the length should be an error. CHADRON: shoud NOT be
+	{[]byte{0xa0, 0x82, 0x00, 0xff}, true, tagAndLength{2, 0, 255, true}},
 	// Lengths up to the maximum size of an int should work.
 	{[]byte{0xa0, 0x84, 0x7f, 0xff, 0xff, 0xff}, true, tagAndLength{2, 0, 0x7fffffff, true}},
 	// Lengths that would overflow an int should be rejected.
 	{[]byte{0xa0, 0x84, 0x80, 0x00, 0x00, 0x00}, false, tagAndLength{}},
-	// Long length form may not be used for lengths that fit in short form.
-	{[]byte{0xa0, 0x81, 0x7f}, false, tagAndLength{}},
+	// Long length form may not be used for lengths that fit in short form. CHADRON: CAN
+	{[]byte{0xa0, 0x81, 0x7f}, true, tagAndLength{2, 0, 127, true}},
 	// Tag numbers which would overflow int32 are rejected. (The value below is 2^31.)
 	{[]byte{0x1f, 0x88, 0x80, 0x80, 0x80, 0x00, 0x00}, false, tagAndLength{}},
 	// Tag numbers that fit in an int32 are valid. (The value below is 2^31 - 1.)
 	{[]byte{0x1f, 0x87, 0xFF, 0xFF, 0xFF, 0x7F, 0x00}, true, tagAndLength{tag: math.MaxInt32}},
-	// Long tag number form may not be used for tags that fit in short form.
-	{[]byte{0x1f, 0x1e, 0x00}, false, tagAndLength{}},
+	// Long tag number form may not be used for tags that fit in short form. CHADRON: MAY
+	{[]byte{0x1f, 0x1e, 0x00}, true, tagAndLength{0, 30, 0, false}},
 }
 
 func TestParseTagAndLength(t *testing.T) {
